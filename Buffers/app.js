040932e7d8e3dcb2d3e611ecc2189644fa7361dc
SaptatirthaChoudhury <saptatirtha@gmail.com>
2025-03-07 02:49:41 +0530

    Buffers: more buffer method

--- Buffers/app.js
+++ Buffers/app.js
@@ -1,5 +1,5 @@
 const { Buffer } = require("buffer");
-
+/*
 const memoryContainer = Buffer.alloc(5); // 5 bytes (40 bits)
 
 console.log(memoryContainer); // If we log this container then we'll see all the elements are initialized to zero
@@ -8,7 +8,7 @@ memoryContainer[0] = 0xf4;
 memoryContainer[1] = 0x34;
 memoryContainer.writeInt8(-34, 2);
 memoryContainer[3] = 0xb6;
-memoryContainer[4] = 0b0110;
+memoryContainer[4] = 0x21;
 
 
 console.log(memoryContainer);
@@ -18,6 +18,40 @@ console.log(memoryContainer.readInt8(2));
 console.log(memoryContainer[3]);
 console.log(memoryContainer[4]);
 
+console.log(memoryContainer.toString("utf-8"));
+
+*/
+
+/*
+const buff1 = Buffer.from([0x48, 0x69, 0x21])
+console.log(buff1);
+console.log(buff1.toString("utf-8"));
+
+const buff2 = Buffer.from("486921", "hex")
+console.log(buff2);
+console.log(buff2.toString("utf-8"));
+
+const buff3 = Buffer.from("string", 'utf-8')
+console.log(buff3);
+console.log(buff3.toString("utf-8"));
+
+const buff4 = Buffer.from("10010", "binary")
+console.log(buff4);
+console.log(buff4.toString("utf-8"));
+
+*/
+// const num = parseInt("10010", 2)
+// console.log(num);
+// const buff5 = Buffer.from([num])
+// console.log(buff5);
+
+// const buff = Buffer.from("Hi!", "utf-8")
+// console.log(buff);
+
+const buff = Buffer.from("E0A69D", "binary")
+console.log(buff)
+const back = buff.toString("utf-8")
+console.log(back);
+console.log(Buffer.from(back, "hex").toString("utf-8"));
 
 
-console.log(memoryContainer[4].toString("binary"));
